cd12cd4c094c6c010b557dad85e35e51a631155c
luozhengjiafei <luozhengjiafei@gmail.com>
2020-08-17 21:30:22 -0700

    Updates

---
+++ precache-manifest.0791b8172baa82ae5ed9dda05fb0bf41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7ca9e17fa4a81a696d859f0e3ffcc20",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "c5d199daccc7a01b6907",
+    "url": "/Calculator/static/css/main.bccc640c.chunk.css"
+  },
+  {
+    "revision": "039ce1c5b65c61f0dcfd",
+    "url": "/Calculator/static/js/2.00e2c55f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.00e2c55f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d199daccc7a01b6907",
+    "url": "/Calculator/static/js/main.d6c1be86.chunk.js"
+  },
+  {
+    "revision": "45e80991cacdf9252f8e",
+    "url": "/Calculator/static/js/runtime-main.7c3b03a7.js"
+  }
+]);
